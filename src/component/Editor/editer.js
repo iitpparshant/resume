2a0f574c8b25917e5c5d03bb7f13ee01d9e6e37d
iitpparshant <parshantverma27.2003@gmail.com>
2023-12-08 23:08:30 +0530

    all

--- src/component/Editor/editer.js
+++ src/component/Editor/editer.js
@@ -3,7 +3,7 @@ import "./editor1.css"
 function Editor(props) {
     const sections= props.sections;
     return (
-        <div className="container">
+        <div className="container2">
             <div className="header">
                 {Object.keys(sections)?.map((key)=>(
                     <div className="section" key= {key}>
