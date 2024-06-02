2bf3366e2ef64e3321ca4309c13e36c732f94583
iitpparshant <parshantverma27.2003@gmail.com>
2024-06-02 17:54:52 +0530

    update

--- fronted/src/component/header/header.js
+++ fronted/src/component/header/header.js
@@ -3,23 +3,27 @@ import "./header1.css";
 import resumesvg from "../assets/resume.svg";
 import clone from "../assets/clone.jpeg"
 import { Link } from "react-router-dom";
+import arrow from "../assets/arrow.gif"
 function Header() {
     return (
         <div>
 
-        <div className="container1">
-            <div>
-                <h1 className="head">A <span>Resume</span> that stands out!</h1>
-                <p className="head">Make your own resume. <span>It is free</span></p>
-                <h1 className="resume">For making your resume click on the below button</h1>
-                <Link className="click" to="/body">Click Here</Link>
+            <div className="container1">
+                <div className="cont">
+                    <h1 className="head">A <span>Resume</span> that stands out!</h1>
+                    <p className="head">Make your own resume. <span>It is free</span></p>
+                    <h1 className="resume">For making your resume click on the below button</h1>
+                    <div className="arrowclick">
+                        <img className="arrow" alt="" src={arrow} />
+                        <Link className="click" to="/body">Click Here</Link>
+                    </div>
+                </div>
+                <div className="right">
+                    <img src={resumesvg} alt="" />
+                </div>
             </div>
-            <div className="right">
-                <img src={resumesvg} alt="" />
-            </div>
-        </div>
             <div className="templet">
-                <img src={clone} alt=""/>
+                <img src={clone} alt="" />
                 <h1>STUDENT RESUME TEMPLET</h1>
             </div>
         </div>
