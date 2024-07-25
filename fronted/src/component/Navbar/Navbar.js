12ad4bdd2f866b6f65642631ffca8b5a085a7953
iitpparshant <parshantverma27.2003@gmail.com>
2024-07-25 11:16:46 +0530

    update

--- fronted/src/component/Navbar/Navbar.js
+++ fronted/src/component/Navbar/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
   const navigate = useNavigate();
   const handleLogout = () => {
     localStorage.removeItem("authToken")
-    localStorage.removeItem("email")
+    localStorage.removeItem("userEmail")
     navigate("/signin")
   }
 
